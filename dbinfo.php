4b072c1ce431522a15b37af364df3b4cf1abe688
mukul007ju <mukul.007ju@gmail.com>
2023-07-31 20:46:01 +0600

    Add files via upload

---
+++ dbinfo.php
@@ -0,0 +1,13 @@
+<?php
+//sql server
+//$dbhost = "localhost";
+//$dbuser = "root";
+//$dbpassword = "1234";
+//$dbname = "test";
+
+//postgis server
+$remote_dbhost = "192.168.0.109";
+$remote_dbuser = "postgres";
+$remote_dbpassword = "1234";
+$remote_dbname = "morshed";
+?>
---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  // AJAX request to load GeoJSON data
+  $.ajax({
+    type: 'GET',
+    url: 'http://localhost/JSON_JS/chatgpt/postgis2geojson.php',
+    dataType: 'json',
+    success: function(data) {
+      // Data received successfully, let's populate the table
+      if (data.features && data.features.length > 0) {
+        var tableBody = $('#table-body');
+        $.each(data.features, function(index, features) {
+          var plot_no = features.properties.plot_no;
+          var landuse = features.properties.landuse;
+          var katha = features.properties.katha;
+          
+          // Append data to the table
+          var row = '<tr>' +
+                      '<td>' + plot_no + '</td>' +
+                      '<td>' + landuse + '</td>' +
+                      '<td>' + katha + '</td>' +
+					  
+					  
+                    '</tr>';
+          tableBody.append(row);
+        });
+      }
+    },
+    error: function(xhr, status, error) {
+      console.error('Error loading GeoJSON data:', error);
+    }
+  });
+});
